debcfa81c2324259e635f6b0211d503b9b7ad62f
Callum Wilson <cwilson19902004@hotmail.com>
2019-02-06 17:52:10 +0000

    removal of platform files

--- platforms/ios/Pods/Target Support Files/Pods-shoppinglist/Pods-shoppinglist-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_shoppinglist : NSObject
-@end
-@implementation PodsDummy_Pods_shoppinglist
-@end
--- platforms/ios/build/shoppinglist.build/Debug-iphonesimulator/shoppinglist.build/Objects-normal/x86_64/NativeScriptEmbedder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/rebeca/Callum_uni/shopping_list/shopping_list/platforms/ios/shoppinglist/shoppinglist-Prefix.pch \
-  /Users/rebeca/Callum_uni/shopping_list/shopping_list/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.m \
-  /Users/rebeca/Callum_uni/shopping_list/shopping_list/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.h
