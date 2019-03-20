09acb2fe8dcbe7ffabcf2c34b4722fa6f0ede9bd
Callum Wilson <cwilson19902004@hotmail.com>
2019-03-20 16:56:57 +0000

    changed login gui elements

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { alert, prompt } from "tns-core-modules/ui/dialogs";
 import { TextField } from "tns-core-modules/ui/text-field";
@@ -9,79 +9,104 @@ import { RouterExtensions } from "nativescript-angular/router";
 import * as firebase from "nativescript-plugin-firebase";
 import * as localStorage from "nativescript-localstorage";
 import { Router } from "@angular/router";
+import { registerContentQuery } from "@angular/core/src/render3";
 
 
 @Component({
-    selector: "Login",
-    moduleId: module.id,
-    templateUrl: "./login.component.html",
-    styleUrls: ['./login.component.css'],
+  selector: "Login",
+  moduleId: module.id,
+  templateUrl: "./login.component.html",
+  styleUrls: ['./login.component.css'],
 })
+
 
 export class LoginComponent implements OnInit {
 
-    user: User;
-    
-    constructor(private page: Page, private routerExtensions: RouterExtensions) {
-        // Use the component constructor to inject providers.
-        this.page.actionBarHidden = true;
-    }
+  constructor(private page: Page, private routerExtensions: RouterExtensions, private zone: NgZone) {
+    // Use the component constructor to inject providers.
+    this.page.actionBarHidden = true;
+  }
 
-    ngOnInit(): void {
-        // Init your component properties here.
-    }
+  ngOnInit(): void {
+    // Init your component properties here.
+  }
 
-    onDrawerButtonTap(): void {
-        const sideDrawer = <RadSideDrawer>app.getRootView();
-        sideDrawer.showDrawer();
-    }
+  onDrawerButtonTap(): void {
+    const sideDrawer = <RadSideDrawer>app.getRootView();
+    sideDrawer.showDrawer();
+  }
 
-        //firebase login with google
-        google() {
-            firebase.login({
-                type: firebase.LoginType.GOOGLE,
-                // Optional 
-                googleOptions: {
-                  hostedDomain: "mygsuitedomain.com"
-                }
-              }).then(
-                  function (result) {
-                    JSON.stringify(result);
-                    console.log(result.email);
-                    console.log(result.uid);
-                    //this.user = new User(result.email, result.name);
-                    //this.user.isLoggedIn = true;
-                    //localStorage.setItemObject('user', JSON.stringify(this.user));
-                    //this.routerExtensions.navigate(["/home"], { clearHistory: true});
-                  },
-                  function (errorMessage) {
-                    console.log(errorMessage);
-                  }
-              );
+  bypassLogin(){
+
+    this.routerExtensions.navigate(['/home'], { clearHistory: true });
+  }
+
+  //firebase login with google
+  google() {
+    firebase.login({
+      type: firebase.LoginType.GOOGLE,
+      // Optional 
+      googleOptions: {
+        hostedDomain: "mygsuitedomain.com"
+      }
+    }).then(
+      function (user) {
+        JSON.stringify(user);
+        console.log(user.email);
+        console.log(user.uid);
+        //this.user = new User(user.email, user.name);
+        //this.user.isLoggedIn = true;
+        //localStorage.setItemObject('user', JSON.stringify(this.user));
+        //this.routerExtensions.navigate(["/home"], { clearHistory: true});
+      },
+      function (errorMessage) {
+        console.log(errorMessage);
+      }
+    );
+  }
+
+  //FOR TESTING ONLY
+  anon() {
+
+    firebase.login(
+      {
+        type: firebase.LoginType.ANONYMOUS
+      })
+      .then(
+        function (user) {
+          JSON.stringify(user);
+          console.log("User ID: " + user.uid);
         }
-    
-        //firebase login with facebook
-        facebook() {
-            firebase.login({
-                type: firebase.LoginType.FACEBOOK,
-                // Optional
-                facebookOptions: {
-                  // defaults to ['public_profile', 'email']
-                  scope: ['public_profile', 'email']
-                }
-              }).then(
-                  function (result) {
-                    JSON.stringify(result);
-                    console.log(result.email);
-                    console.log(result.uid);
-                    //this.user = new User(result.email, result.name);
-                    //this.user.isLoggedIn = true;
-                    //localStorage.setItemObject('user', JSON.stringify(this.user));
-                    //this.routerExtensions.navigate(["/home"], { clearHistory: true});
-                  },
-                  function (errorMessage) {
-                    console.log(errorMessage);
-                  }
-              );
+      )
+      .catch(
+        function (errorMessage) {
+          console.log(errorMessage);
         }
+      )
+  }
+
+  //firebase login with facebook
+  facebook() {
+    firebase.login({
+      type: firebase.LoginType.FACEBOOK,
+      // Optional
+      facebookOptions: {
+        // defaults to ['public_profile', 'email']
+        scope: ['public_profile', 'email']
+      }
+    }).then(
+      function (user) {
+        JSON.stringify(user);
+        console.log(user.email);
+        console.log(user.uid);
+        //this.user = new User(user.email, user.name);
+        //this.user.isLoggedIn = true;
+        //localStorage.setItemObject('user', JSON.stringify(this.user));
+        //this.routerExtensions.navigate(["/home"], { clearHistory: true});
+      },
+      function (errorMessage) {
+        console.log(errorMessage);
+      }
+    );
+  }
 }
--- src/app/shared/user.model.ts
+++ src/app/shared/user.model.ts
@@ -1,14 +1,22 @@
+//ToDo - update model to match return values from firebase
+
 export class User {
 
-    public email: string;
-    public password: string;
-    public confirmPassword: string;
-    public username: string;
-    public memberType: string;
+   public uid: string;
+   public anonymous: boolean;
+   public emailVerified: boolean;
+   public providers;
+   public email: string;
+   public name: string;
+   public profileImageURL: string;
+   public isNewUser: boolean;
+   public providerId: string;
+   public creationTimeStamp: Date;
+   public lastSignInTimeStamp: Date;
 
-    constructor(email, username) {
-        
-        this.email = email;
-        this.username = username;
+    constructor() {
+
     }
+
 }
+
