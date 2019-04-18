c5b4e3cb30fb7796a865d3eb6d507f2c2b5f4dcc
Ryan Thompson <1624600@student.fife.ac.uk>
2019-04-18 23:28:55 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import * as platform from "platform";
 declare var GMSServices: any;
 
 if (platform.isIOS) { 
-    GMSServices.provideAPIKey("AIzaSyAJ_ZYLoGFy9LiBBAVM0ArWvjWd3NXK82Q");
+    GMSServices.provideAPIKey("");
   }
 @NgModule({
     bootstrap: [
